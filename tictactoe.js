eec8c927eba7c20e7b074e0fe243a32533a9bb14
Meekology <22243974+AwzemCoffee@users.noreply.github.com>
2026-06-18 10:53:41 -0600

    Structure and logic fixes
    Logic add for getting turn count and ties

--- tictactoe.js
+++ tictactoe.js
@@ -2,15 +2,16 @@ const gameBoard = (() => {
     let boardSpaces = [];
     for (let spaces = 0; spaces < 9; ++spaces) {
         const newSpace = (() => {
-            let isOccupied = null;
+            let isOccupied = false;
 
             const getOccupied = () => {
-                return isOccupied != null;
+                return isOccupied;
             }
 
             const setOccupied = (activePlayer) => {
                 if (getOccupied() === false) {
                     isOccupied = activePlayer;
+                    gameController.incrementTurn();
                     return true;
                 } else return false;
             }
@@ -21,17 +22,20 @@ const gameBoard = (() => {
     }
 
     const updateSpace = (activePlayer, index) => {
-        return boardSpaces[index].setOccupied(activePlayer);
+        boardSpaces[index].setOccupied(activePlayer);
     }
 
-    return { updateSpace };
+    const isSpaceOccupied = (index) => {
+        return boardSpaces[index].getOccupied() ? true : false;
+    }
+
+    return { updateSpace, isSpaceOccupied };
 })();
 
 const gameController = (() => {
-    let turnCount = 0;
+
     const playerOne = createPlayer("John", "X");
     const playerTwo = createPlayer("Jarod", "O");
-
     let activePlayer = playerOne;
 
     function createPlayer(name, marker) {
@@ -39,13 +43,30 @@ const gameController = (() => {
         const playerMarker = marker;
         return { playerName, playerMarker };
     }
+
     const incrementTurn = () => {
-        turnCount = + 1;
+        turnCount += 1;
+        switchActivePlayer();
+    }
+    let turnCount = 0;
+
+    const getActivePlayer = () => {
+        return activePlayer;
     }
 
     const switchActivePlayer = () => {
-        activePlayer = playerOne ? playerTwo : playerOne
+        activePlayer = (playerOne) ? playerTwo : playerOne
     }
 
-    return { incrementTurn, switchActivePlayer }
+    const currentTurnCount = () => {
+        return turnCount;
+    }
+
+    const checkWinCondition = () => {
+        if (currentTurnCount === 8) {
+            return console.log("Tie")
+        }
+    }
+
+    return { incrementTurn, getActivePlayer, currentTurnCount };
 })();
