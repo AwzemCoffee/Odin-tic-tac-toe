7c100f4188dcd480384f57bf990792957ea4affd
Meekology <22243974+AwzemCoffee@users.noreply.github.com>
2026-06-21 11:07:56 -0600

    Add win condition & check logic
    There is now win & tie conditions for completing the game

--- tictactoe.js
+++ tictactoe.js
@@ -1,72 +1,85 @@
-const gameBoard = (() => {
-    let boardSpaces = [];
-    for (let spaces = 0; spaces < 9; ++spaces) {
-        const newSpace = (() => {
-            let isOccupied = false;
-
-            const getOccupied = () => {
-                return isOccupied;
-            }
-
-            const setOccupied = (activePlayer) => {
-                if (getOccupied() === false) {
-                    isOccupied = activePlayer;
-                    gameController.incrementTurn();
-                    return true;
-                } else return false;
-            }
-
-            return { getOccupied, setOccupied };
-        })();
-        boardSpaces.push(newSpace);
-    }
-
-    const updateSpace = (activePlayer, index) => {
-        boardSpaces[index].setOccupied(activePlayer);
-    }
-
-    const isSpaceOccupied = (index) => {
-        return boardSpaces[index].getOccupied() ? true : false;
-    }
-
-    return { updateSpace, isSpaceOccupied };
-})();
 
 const gameController = (() => {
+
+    const gameBoard = (() => {
+        let spaces = new Array(8).fill(null);
+        return { spaces };
+    })();
+
+    const createPlayer = (name, mark) => {
+        const playerName = name;
+        const playerMark = mark;
+        return { playerName, playerMark };
+    }
 
     const playerOne = createPlayer("John", "X");
     const playerTwo = createPlayer("Jarod", "O");
     let activePlayer = playerOne;
+    let turnCount = 1;
 
-    function createPlayer(name, marker) {
-        const playerName = name;
-        const playerMarker = marker;
-        return { playerName, playerMarker };
+    function setActivePlayer() {
+        return activePlayer = (activePlayer === playerOne) ? playerTwo : playerOne;
     }
 
-    const incrementTurn = () => {
+    function incrementTurn() {
         turnCount += 1;
-        switchActivePlayer();
-    }
-    let turnCount = 0;
-
-    const getActivePlayer = () => {
-        return activePlayer;
     }
 
-    const switchActivePlayer = () => {
-        activePlayer = (playerOne) ? playerTwo : playerOne
-    }
-
-    const currentTurnCount = () => {
+    function getTurnCount() {
         return turnCount;
     }
 
-    const checkWinCondition = () => {
-        if (currentTurnCount === 8) {
-            return console.log("Tie")
+    function isOccupied(index) {
+        if (gameBoard.spaces[index] !== null) {
+            return true;
+        } else return false;
+    }
+
+    function populateSpace(index) {
+        if (isOccupied(index) === false) {
+            gameBoard.spaces[index] = activePlayer.playerMark;
+            checkWinCondition();
+            incrementTurn();
+            setActivePlayer();
+        } else console.log("Already Occupied")
+    }
+
+    function getSpaces(index) {
+        return gameBoard.spaces(index);
+    }
+
+    function getAllSpaces() {
+        console.table(gameBoard.spaces)
+    }
+
+    function checkWinCondition() {
+        if (getTurnCount() === 9) {
+            console.log("Tie")
+        } else {
+            const answerKey = [
+                [0, 4, 8],
+                [2, 4, 6],
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8],
+            ];
+            let winner = null;
+            let wonBoolean = answerKey.some((ansKey) => {
+                return ansKey.every((index) => {
+                    return gameBoard.spaces[index] === activePlayer.playerMark;
+                });
+            });
+            if (wonBoolean === true) {
+                console.log(`${activePlayer.playerName} has won!`);
+                winner = activePlayer.playerName;
+            } else {
+                console.log(`No winner yet`);
+            }
         }
     }
 
-    return { incrementTurn, getActivePlayer, currentTurnCount };
+    return { getTurnCount, populateSpace, getSpaces, getAllSpaces }
 })();
